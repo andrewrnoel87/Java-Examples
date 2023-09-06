29471af4f48c4d6638eb7ae80985200b90a58678
andrewrnoel87 <144265546+andrewrnoel87@users.noreply.github.com>
2023-09-06 12:58:02 -0400

    first upload of work with Head First Java

---
+++ headfirst/page285/src/Foo.java
@@ -0,0 +1,9 @@
+public class Foo{
+	public static void main (String[] args) {
+	}
+	static int x;
+	
+	public void go() {
+		System.out.println(x);
+	}
+}
---
+++ headfirst/page285/src/Foo4.java
@@ -0,0 +1,10 @@
+public class Foo4 {
+	public static void main (String[] Args) {
+	}
+	
+	static final int x = 12;
+	
+	public void go() {
+		System.out.println(x);
+	}
+}
